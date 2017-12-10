a2bf2bda6be262601a3f054185b9d7304fa19211
Roman <r.i.bogdanov1@gmail.com>
2017-12-10 14:19:58 +0300

    MainWindow done - added event handlers FunctionWindow done - layout & handlers

---
+++ GUI/FunctionWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Логика взаимодействия для FunctionWindow.xaml
+    /// </summary>
+    public partial class FunctionWindow : Window
+    {
+        public Window BooksWindow { get; set; }
+        public Window AuthorsWindow { get; set; }
+        public Window StoragePlacesWindow { get; set; }
+
+        public int FunctionIndicator { get; set; }
+
+        public FunctionWindow(int tag, string title)
+        {
+            InitializeComponent();
+            FunctionIndicator = tag;
+            TitleTextBlock.Text = title;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var tag = button.Tag as string;
+            var tagInt = Convert.ToInt32(tag);
+            switch (tagInt)
+            {
+                case 0:
+                    //нажата кнопка "Книги"
+                    switch (FunctionIndicator) 
+                        //Инициализация объектов разных классов, в зависимости от функции:
+                        //0 - read
+                        //1 - create
+                        //2 - edit & delete
+                    {
+                        case 0:
+                            //BooksWindow = new ...
+                            break;
+                        case 1:
+                            //BooksWindow = new ...
+                            break;
+                        case 2:
+                            //BooksWindow = new ...
+                            break;
+                    }
+                    break;
+                case 1:
+                    //нажата кнопка "Авторы"
+                    switch (FunctionIndicator)
+                    {
+                        //Инициализация объектов разных классов, в зависимости от функции:
+                        //0 - read
+                        //1 - create
+                        //2 - edit & delete
+                        case 0:
+                            //AuthorsWindow = new ...
+                            break;
+                        case 1:
+                            //AuthorsWindow = new ...
+                            break;
+                        case 2:
+                            //AuthorsWindow = new ...
+                            break;
+                    }
+                    break;
+                case 2:
+                    //нажата кнопка "Места хранения"
+                    switch (FunctionIndicator)
+                    {
+                        //Инициализация объектов разных классов, в зависимости от функции:
+                        //0 - read
+                        //1 - create
+                        //2 - edit & delete
+                        case 0:
+                            //StoragePlacesWindow = new ...
+                            break;
+                        case 1:
+                            //StoragePlacesWindow = new ...
+                            break;
+                        case 2:
+                            //StoragePlacesWindow = new ...
+                            break;
+                    }
+                    break;
+                case 3:
+                    //нажата кнопка "Назад"
+                    Close();
+                    break;
+            }
+        }
+    }
+}
--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -20,14 +20,38 @@ namespace GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public FunctionWindow ReadWindow { get; set; }
+        public FunctionWindow CreateWindow { get; set; }
+        public FunctionWindow EditDeleteWindow { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Close();
+            var button = sender as Button;
+            var tag = button.Tag as string;
+            var tagInt = Convert.ToInt32(tag);
+            switch (tagInt)
+            {
+                case 0:
+                    ReadWindow = new FunctionWindow(tagInt, "Просмотреть данные:");
+                    ReadWindow.Show();
+                    break;
+                case 1:
+                    CreateWindow = new FunctionWindow(tagInt, "Добавить новые данные:");
+                    CreateWindow.Show();
+                    break;
+                case 2:
+                    EditDeleteWindow = new FunctionWindow(tagInt, "Редактировать и удалить данные:");
+                    EditDeleteWindow.Show();
+                    break;
+                case 3:
+                    Close();
+                    break;
+            }
         }
     }
 }
